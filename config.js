da3ae460e007f52060a5f5bd8435f4df9dbe7b9b
anubhav <anubhav.balodhi@gmail.com>
2020-09-10 13:14:17 +0530

    added mongodb connection

--- config.js
+++ config.js
@@ -5,8 +5,10 @@ var path = require('path');
  */
 function connectToDB() {
     var mongoose = require('mongoose');
+    var mdb = 'mongodb+srv://madmin:4E2F7pCNb8fMxex@cluster0.sm7iv.gcp.mongodb.net/simulator?retryWrites=true&w=majority';
 
-    mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/simulator', function (err) {
+
+    mongoose.connect(process.env.MONGODB_URI || mdb || 'mongodb://127.0.0.1:27017/simulator', function (err) {
         if (err) {
             console.log('connection error', err);
         } else {
